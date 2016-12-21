c10709e534a0d4edc88ffc9b51876b506935b920
hedo.junior <hedo.junior@cwi.com.br>
2016-12-21 18:38:45 -0200

    name

--- a.py
+++ a.py
@@ -10,7 +10,7 @@ req = {
 "actionIncomplete": False,
 "parameters": {
     "cpf": {
-    "number": 767
+    "number": 1233
     }
 },
 "contexts": [
@@ -52,7 +52,7 @@ req = {
     "parameters": {
         "cpf.original": "767",
         "cpf": {
-        "number": 767,
+        "number": 123,
         "number.original": "767"
         }
     },
@@ -63,7 +63,7 @@ req = {
     "parameters": {
         "cpf.original": "767",
         "cpf": {
-        "number": 767,
+        "number": 123,
         "number.original": "767"
         }
     },
@@ -105,11 +105,14 @@ cpf = int(req.get('result').get('parameters').get('cpf'
 print(cpf)
 
 arr = req.get('result').get('contexts')
+print(len(arr))
 if cpf is 123:
     speech = \
         ' Ah, ent\xc3\xa3o voc\xc3\xaa j\xc3\xa1 \xc3\xa9 cliente!,Agora preciso confirmar alguns dados com voc\xc3\xaa: \n                     O INSS|SIAPE|ETC continua sendo sua fonte pagadora?'
-    arr = [v for v in arr if v.get('name') != ctx and v.get(name) != ctx1 ]
+    arr = [v for v in arr if v.get('name') != ctx and v.get('name') != ctx1 ]
 else:
     speech = \
         ' Voc\xc3\xaa ainda n\xc3\xa3o \xc3\xa9 cliente do Banco Agiplan?,Ent\xc3\xa3o seja bem vindo!,\n                     Voc\xc3\xaa \xc3\xa9 funcion\xc3\xa1rio p\xc3\xbablico, aposentado ou pensionista?'
     arr = [v for v in arr if v.get('name') != ctx2 ]
+
+print(len(arr))
--- app.py
+++ app.py
@@ -98,7 +98,7 @@ def makeWebhookResult(req):
     if cpf is 123:
         speech = \
             ' Ah, ent\xc3\xa3o voc\xc3\xaa j\xc3\xa1 \xc3\xa9 cliente!,Agora preciso confirmar alguns dados com voc\xc3\xaa: \n                     O INSS|SIAPE|ETC continua sendo sua fonte pagadora?'
-        arr = [v for v in arr if v.get('name') != ctx and v.get(name) != ctx1 ]
+        arr = [v for v in arr if v.get('name') != ctx and v.get('name') != ctx1 ]
     else:
         speech = \
             ' Voc\xc3\xaa ainda n\xc3\xa3o \xc3\xa9 cliente do Banco Agiplan?,Ent\xc3\xa3o seja bem vindo!,\n                     Voc\xc3\xaa \xc3\xa9 funcion\xc3\xa1rio p\xc3\xbablico, aposentado ou pensionista?'
