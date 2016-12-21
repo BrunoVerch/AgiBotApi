7806d6d9eb30c492ebaac25fcef3f0a2e2829c92
Bruno da Silva Verch <bruno.verch@cwi.com.br>
2016-12-21 15:43:45 -0200

    ..

--- app.py
+++ app.py
@@ -31,14 +31,14 @@ def webhook():
 def processRequest(req):
     if req.get("result").get("action") != "verificarCpf":
         return {}
-    baseurl = "https://query.yahooapis.com/v1/public/yql?"
-    yql_query = makeYqlQuery(req)
-    if yql_query is None:
-        return {}
-    yql_url = baseurl + urllib.urlencode({'q': yql_query}) + "&format=json"
-    result = urllib.urlopen(yql_url).read()
-    data = json.loads(result)
-    res = makeWebhookResult(data)
+    # baseurl = "https://query.yahooapis.com/v1/public/yql?"
+    # yql_query = makeYqlQuery(req)
+    # if yql_query is None:
+    #     return {}
+    # yql_url = baseurl + urllib.urlencode({'q': yql_query}) + "&format=json"
+    # result = urllib.urlopen(yql_url).read()
+    # data = json.loads(result)
+    # res = makeWebhookResult(data)
 
     return {
         "speech": "apiai-weather-webhook-sample",
