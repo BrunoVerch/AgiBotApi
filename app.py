6a1d8b2e8263b4364106a42286bf32a65c73d8c9
hedo.junior <hedo.junior@cwi.com.br>
2016-12-21 18:47:15 -0200

    strlenarr

--- app.py
+++ app.py
@@ -97,13 +97,13 @@ def makeWebhookResult(req):
     arr = req.get('result').get('contexts')
     if cpf is 123:
         arr = [v for v in arr if v.get('name') != ctx and v.get('name') != ctx1 ]
-        speech = \
-            len(arr) + ' Ah, ent\xc3\xa3o voc\xc3\xaa j\xc3\xa1 \xc3\xa9 cliente!,Agora preciso confirmar alguns dados com voc\xc3\xaa: \n                     O INSS|SIAPE|ETC continua sendo sua fonte pagadora?'
+        speech = str(len(arr))# \
+             #+ ' Ah, ent\xc3\xa3o voc\xc3\xaa j\xc3\xa1 \xc3\xa9 cliente!,Agora preciso confirmar alguns dados com voc\xc3\xaa: \n                     O INSS|SIAPE|ETC continua sendo sua fonte pagadora?'
         
     else:
         arr = [v for v in arr if v.get('name') != ctx2 ]
-        speech = \
-            len(arr) + ' Voc\xc3\xaa ainda n\xc3\xa3o \xc3\xa9 cliente do Banco Agiplan?,Ent\xc3\xa3o seja bem vindo!,\n                     Voc\xc3\xaa \xc3\xa9 funcion\xc3\xa1rio p\xc3\xbablico, aposentado ou pensionista?'
+        speech = str(len(arr))#\
+            #len(arr) + ' Voc\xc3\xaa ainda n\xc3\xa3o \xc3\xa9 cliente do Banco Agiplan?,Ent\xc3\xa3o seja bem vindo!,\n                     Voc\xc3\xaa \xc3\xa9 funcion\xc3\xa1rio p\xc3\xbablico, aposentado ou pensionista?'
 
     return {  # "data": data,
         'speech': speech,
