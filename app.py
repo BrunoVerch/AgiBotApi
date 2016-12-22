cbf3234831a8532bad4fc85aa01fc11869d25085
Bruno da Silva Verch <bruno.verch@cwi.com.br>
2016-12-22 14:38:45 -0200

    Refatoracao

--- app.py
+++ app.py
@@ -13,19 +13,19 @@ from flask import make_response
 
 app = Flask(__name__)
 
+contexts = [
+    'creditoconsignado-solicitacaoemprestimo-solicitarperfilcliente-épublicoalvo'
+    'creditoconsignado-solicitacaoemprestimo-solicitarperfilcliente-naoépublicoalvo'
+    'creditoconsignado-solicitacaoemprestimo-confirmardadoscliente'
+]
 
 @app.route('/webhook', methods=['POST'])
 def webhook():
     req = request.get_json(silent=True, force=True)
 
-    print 'Request:'
-    print json.dumps(req, indent=4)
-
     res = processRequest(req)
 
     res = json.dumps(res, indent=4)
-
-    # print(res)
 
     r = make_response(res)
     r.headers['Content-Type'] = 'application/json'
@@ -33,91 +33,42 @@ def webhook():
 
 
 def processRequest(req):
-    if req.get('result').get('action') != 'verificarCpf':
-        return {}
-    #baseurl = 'https://query.yahooapis.com/v1/public/yql?'
-    #yql_query = makeYqlQuery(req)
-    #if yql_query is None:
-    #    return {}
-    #yql_url = baseurl + urllib.urlencode({'q': yql_query}) \
-    #    + '&format=json'
-    #result = urllib.urlopen(yql_url).read()
-    #data = json.loads(result)
-    res = makeWebhookResult(req)
-    return res
+    action = req.get('result').get('action')
 
+    if action == 'verificarCpf':
+        return verificarCpf(req)
+    if action == 'efetuarSimulacao':
+        return efetuarSimulacao(req)
+    
+    return {}
 
-def makeYqlQuery(req):
-    result = req.get('result')
-    parameters = result.get('parameters')
-    city = parameters.get('cpf')
-    if city is None:
-        return None
+def efetuarSimulacao(req):
+    return ''
 
-    return "select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='" \
-        + city + "')"
+def verificarCpf(req):
+    textoSaida = ''
+    contextosDeSaida = []
 
+    cpf = int(req.get('result').get('parameters').get('cpf').get('number'))
 
-def makeWebhookResult(req):
-    # query = data.get('query')
-    # if query is None:
-    #     return {}
-
-    # result = query.get('results')
-    # if result is None:
-    #     return {}
-
-    # channel = result.get('channel')
-    # if channel is None:
-    #     return {}
-
-    # item = channel.get('item')
-    # location = channel.get('location')
-    # units = channel.get('units')
-    # if location is None or item is None or units is None:
-    #     return {}
-
-    # condition = item.get('condition')
-    # if condition is None:
-    #     return {}
-
-    # # print(json.dumps(item, indent=4))
-
-    # speech = 'Today in ' + location.get('city') + ': ' \
-    #     + condition.get('text') + ', the temperature is ' \
-    #     + condition.get('temp') + ' ' + units.get('temperature')
-    speech = 'aa'
-    ctx = 'creditoconsignado-solicitacaoemprestimo-solicitarperfilcliente-épublicoalvo'
-    ctx1 = 'creditoconsignado-solicitacaoemprestimo-solicitarperfilcliente-naoépublicoalvo'
-    ctx2 = 'creditoconsignado-solicitacaoemprestimo-confirmardadoscliente'
-
-    cpf = int(req.get('result').get('parameters').get('cpf'
-              ).get('number'))
-
-    arr = req.get('result').get('contexts')
-    saida = []
-    saida.append({"name":"creditoconsignado-solicitacaoemprestimo-dto", "lifespan":5, "parameters":{}})
     if cpf is 123:
-        # saida = [v for v in arr if v.get('name') != ctx and v.get('name') != ctx1 ]
-        saida.append({"name":"creditoconsignado-solicitacaoemprestimo-confirmardadoscliente", "lifespan":2, "parameters":{}})
-        speech = ' Ah, ent\xc3\xa3o voc\xc3\xaa j\xc3\xa1 \xc3\xa9 cliente!,Agora preciso confirmar alguns dados com voc\xc3\xaa: \n                     O INSS|SIAPE|ETC continua sendo sua fonte pagadora?'        
+        contextosDeSaida.append({"name":"creditoconsignado-solicitacaoemprestimo-confirmardadoscliente", "lifespan":2, "parameters":{}})
+        textoSaida = ' Ah, ent\xc3\xa3o voc\xc3\xaa j\xc3\xa1 \xc3\xa9 cliente!,Agora preciso confirmar alguns dados com voc\xc3\xaa: \n                     O INSS|SIAPE|ETC continua sendo sua fonte pagadora?'        
     else:
-        # saida = [v for v in arr if v.get('name') != ctx2 ]
-        saida.append({"name":"creditoconsignado-solicitacaoemprestimo-solicitarperfilcliente-naoépublicoalvo", "lifespan":3, "parameters":{}})
-        saida.append({"name":"creditoconsignado-solicitacaoemprestimo-solicitarperfilcliente-épublicoalvo", "lifespan":6, "parameters":{}})
-        speech = ' Voc\xc3\xaa ainda n\xc3\xa3o \xc3\xa9 cliente do Banco Agiplan?,Ent\xc3\xa3o seja bem vindo!,\n                     Voc\xc3\xaa \xc3\xa9 funcion\xc3\xa1rio p\xc3\xbablico, aposentado ou pensionista?'
+        contextosDeSaida.append({"name":"creditoconsignado-solicitacaoemprestimo-solicitarperfilcliente-naoépublicoalvo", "lifespan":3, "parameters":{}})
+        contextosDeSaida.append({"name":"creditoconsignado-solicitacaoemprestimo-solicitarperfilcliente-épublicoalvo", "lifespan":6, "parameters":{}})
+        textoSaida = ' Voc\xc3\xaa ainda n\xc3\xa3o \xc3\xa9 cliente do Banco Agiplan?,Ent\xc3\xa3o seja bem vindo!,\n                     Voc\xc3\xaa \xc3\xa9 funcion\xc3\xa1rio p\xc3\xbablico, aposentado ou pensionista?'
 
-    return {  # "data": data,
-        'speech': speech,
-        'displayText': speech,
-        'contextOut': saida,
+    return {
+        'speech': textoSaida,
+        'displayText': textoSaida,
+        'contextOut': contextosDeSaida,
+        # "data": data,
         'source': 'apiai-weather-webhook-sample',
         }
 
 
 if __name__ == '__main__':
     port = int(os.getenv('PORT', 5000))
-
-    print 'Starting app on port %d' % port
 
     app.run(debug=False, port=port, host='0.0.0.0')
