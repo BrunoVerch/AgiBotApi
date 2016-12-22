1ddc3db4253e859e22e4ac3e22e2057c653ae7ec
Bruno da Silva Verch <bruno.verch@cwi.com.br>
2016-12-22 13:58:44 -0200

    .

--- app.py
+++ app.py
@@ -100,14 +100,12 @@ def makeWebhookResult(req):
     if cpf is 123:
         # saida = [v for v in arr if v.get('name') != ctx and v.get('name') != ctx1 ]
         saida.append({"name":"creditoconsignado-solicitacaoemprestimo-confirmardadoscliente", "lifespan":2, "parameters":{}})
-        speech = \
-             + ' Ah, ent\xc3\xa3o voc\xc3\xaa j\xc3\xa1 \xc3\xa9 cliente!,Agora preciso confirmar alguns dados com voc\xc3\xaa: \n                     O INSS|SIAPE|ETC continua sendo sua fonte pagadora?'        
+        speech = ' Ah, ent\xc3\xa3o voc\xc3\xaa j\xc3\xa1 \xc3\xa9 cliente!,Agora preciso confirmar alguns dados com voc\xc3\xaa: \n                     O INSS|SIAPE|ETC continua sendo sua fonte pagadora?'        
     else:
         # saida = [v for v in arr if v.get('name') != ctx2 ]
         saida.append({"name":"creditoconsignado-solicitacaoemprestimo-solicitarperfilcliente-naoépublicoalvo", "lifespan":2, "parameters":{}})
         saida.append({"name":"creditoconsignado-solicitacaoemprestimo-solicitarperfilcliente-épublicoalvo", "lifespan":2, "parameters":{}})
-        speech = \
-             + ' Voc\xc3\xaa ainda n\xc3\xa3o \xc3\xa9 cliente do Banco Agiplan?,Ent\xc3\xa3o seja bem vindo!,\n                     Voc\xc3\xaa \xc3\xa9 funcion\xc3\xa1rio p\xc3\xbablico, aposentado ou pensionista?'
+        speech = ' Voc\xc3\xaa ainda n\xc3\xa3o \xc3\xa9 cliente do Banco Agiplan?,Ent\xc3\xa3o seja bem vindo!,\n                     Voc\xc3\xaa \xc3\xa9 funcion\xc3\xa1rio p\xc3\xbablico, aposentado ou pensionista?'
 
     return {  # "data": data,
         'speech': speech,
