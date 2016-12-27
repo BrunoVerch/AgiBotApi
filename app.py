297d513c098c3a44da7157eb2592bb505d704186
Bruno da Silva Verch <bruno.verch@cwi.com.br>
2016-12-27 16:29:31 -0200

    .

--- app.py
+++ app.py
@@ -13,24 +13,24 @@ from flask import make_response
 
 app = Flask(__name__)
 
-contextos = [
-    'creditoconsignado-solicitacaoemprestimo-solicitarperfilcliente-épublicoalvo',
-    'creditoconsignado-solicitacaoemprestimo-solicitarperfilcliente-naoépublicoalvo',
-    'creditoconsignado-solicitacaoemprestimo-dto',
-    'creditoconsignado-solicitacaoemprestimo-confirmardadoscliente-confirmafontepagadora',
-]
+# contextos = [
+#     'creditoconsignado-solicitacaoemprestimo-solicitarperfilcliente-épublicoalvo',
+#     'creditoconsignado-solicitacaoemprestimo-solicitarperfilcliente-naoépublicoalvo',
+#     'creditoconsignado-solicitacaoemprestimo-dto',
+#     'creditoconsignado-solicitacaoemprestimo-confirmardadoscliente-confirmafontepagadora',
+# ]
 
-@app.route('/webhook', methods=['POST'])
-def webhook():
-    req = request.get_json(silent=True, force=True)
+# @app.route('/webhook', methods=['POST'])
+# def webhook():
+#     req = request.get_json(silent=True, force=True)
 
-    res = processRequest(req)
+#     res = processRequest(req)
 
-    res = json.dumps(res, indent=4)
+#     res = json.dumps(res, indent=4)
 
-    r = make_response(res)
-    r.headers['Content-Type'] = 'application/json'
-    return r
+#     r = make_response(res)
+#     r.headers['Content-Type'] = 'application/json'
+#     return r
 
 @app.route('/verificarCPF', methods=['POST'])
 def verificar_cpf():
@@ -43,18 +43,18 @@ def verificar_cpf():
     r.headers['Content-Type'] = 'application/json'
     return r
 
-def processRequest(req):
-    action = req.get('result').get('action')
+# def processRequest(req):
+#     action = req.get('result').get('action')
 
-    if action == 'verificarCpf':
-        return verificarCpf(req)
-    if action == 'efetuarSimulacao':
-        return efetuarSimulacao(req)
+#     if action == 'verificarCpf':
+#         return verificarCpf(req)
+#     if action == 'efetuarSimulacao':
+#         return efetuarSimulacao(req)
     
-    return {}
+#     return {}
 
-def efetuarSimulacao(req):
-    return ''
+# def efetuarSimulacao(req):
+#     return ''
 
 def verificar_cpf_motion_ai(cpf):
 
@@ -78,41 +78,41 @@ def verificar_cpf_motion_ai(cpf):
     return obj
 
 
-def verificarCpf(req):
-    textoSaida = ''
-    contextosDeSaida = []
+# def verificarCpf(req):
+#     textoSaida = ''
+#     contextosDeSaida = []
 
-    cpf = int(req.get('result').get('parameters').get('cpf').get('number'))
+#     cpf = int(req.get('result').get('parameters').get('cpf').get('number'))
 
-    if cpf is 123:
+#     if cpf is 123:
 
-        req.get('result').get('parameters')['perfilCliente'] = 'aposentado'
-        contextosDeSaida.append({"name":contextos[3], "lifespan":2, "parameters":{}})
+#         req.get('result').get('parameters')['perfilCliente'] = 'aposentado'
+#         contextosDeSaida.append({"name":contextos[3], "lifespan":2, "parameters":{}})
 
-        contextosDeSaida.append({"name":contextos[2], "lifespan":5, "parameters":{ "perfilCliente":"aposentado", "fontePagamento":"INSS" }})
-        contextosDeSaida.append({"name":contextos[0], "lifespan":2, "parameters":{ "perfilCliente2":"aposentado" }})
+#         contextosDeSaida.append({"name":contextos[2], "lifespan":5, "parameters":{ "perfilCliente":"aposentado", "fontePagamento":"INSS" }})
+#         contextosDeSaida.append({"name":contextos[0], "lifespan":2, "parameters":{ "perfilCliente2":"aposentado" }})
 
-        # contextosDaRequisicao = req.get('result').get('contexts')
-        # dto = [v for v in contextosDaRequisicao if v.get('name') == contextos[3]]
-        # contextosDaRequisicao.remove(dto[0])
-        # dto[0].get('parameters')['perfilCliente'] = 'aposentado'
-        # dto[0].get('parameters')['fontePagamento'] = 'INSS'
-        # contextosDaRequisicao.append(dto[0])
+#         # contextosDaRequisicao = req.get('result').get('contexts')
+#         # dto = [v for v in contextosDaRequisicao if v.get('name') == contextos[3]]
+#         # contextosDaRequisicao.remove(dto[0])
+#         # dto[0].get('parameters')['perfilCliente'] = 'aposentado'
+#         # dto[0].get('parameters')['fontePagamento'] = 'INSS'
+#         # contextosDaRequisicao.append(dto[0])
 
-        textoSaida = ' Ah, ent\xc3\xa3o voc\xc3\xaa j\xc3\xa1 \xc3\xa9 cliente!,Agora preciso confirmar alguns dados com voc\xc3\xaa: \n                     O INSS continua sendo sua fonte pagadora?'        
-    else:
-        contextosDeSaida.append({"name":contextos[2], "lifespan":5, "parameters":{}})
-        contextosDeSaida.append({"name":contextos[1], "lifespan":3, "parameters":{}})
-        contextosDeSaida.append({"name":contextos[0], "lifespan":6, "parameters":{}})
-        textoSaida = ' Voc\xc3\xaa ainda n\xc3\xa3o \xc3\xa9 cliente do Banco Agiplan?,Ent\xc3\xa3o seja bem vindo!,\n                     Voc\xc3\xaa \xc3\xa9 funcion\xc3\xa1rio p\xc3\xbablico, aposentado ou pensionista?'
+#         textoSaida = ' Ah, ent\xc3\xa3o voc\xc3\xaa j\xc3\xa1 \xc3\xa9 cliente!,Agora preciso confirmar alguns dados com voc\xc3\xaa: \n                     O INSS continua sendo sua fonte pagadora?'        
+#     else:
+#         contextosDeSaida.append({"name":contextos[2], "lifespan":5, "parameters":{}})
+#         contextosDeSaida.append({"name":contextos[1], "lifespan":3, "parameters":{}})
+#         contextosDeSaida.append({"name":contextos[0], "lifespan":6, "parameters":{}})
+#         textoSaida = ' Voc\xc3\xaa ainda n\xc3\xa3o \xc3\xa9 cliente do Banco Agiplan?,Ent\xc3\xa3o seja bem vindo!,\n                     Voc\xc3\xaa \xc3\xa9 funcion\xc3\xa1rio p\xc3\xbablico, aposentado ou pensionista?'
 
-    return {
-        'speech': textoSaida,
-        'displayText': textoSaida,
-        'contextOut': contextosDeSaida,
-        # "data": data,
-        'source': 'apiai-weather-webhook-sample',
-        }
+#     return {
+#         'speech': textoSaida,
+#         'displayText': textoSaida,
+#         'contextOut': contextosDeSaida,
+#         # "data": data,
+#         'source': 'apiai-weather-webhook-sample',
+#         }
 
 
 if __name__ == '__main__':
