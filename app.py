d04a622eda0d48c30b3770b4eeeac811bfd9d6d9
Bruno da Silva Verch <bruno.verch@cwi.com.br>
2017-01-06 16:23:33 -0200

    .

--- app.py
+++ app.py
@@ -62,6 +62,7 @@ def verificarCpf(req):
         contextosDeSaida.append({"name":contextoSaida, "lifespan":5, "parameters":{ "perfilCliente":"aposentado", "bancoRecebimento": "Banco do Brasil", "ehCliente":True, "fontePagadora":"INSS", "rendaAproximada": "1.950,00", "temSeguroDeVida": False, "temCartao": False, "nome": "Hedo" }})
         textoSaida = ' Ah, então você já é cliente! Agora preciso confirmar alguns dados com você: O INSS continua sendo sua fonte pagadora?'        
     else:
+        contextosDeSaida.append({"name":contextoSaida, "lifespan":5, "parameters":{})
         textoSaida = ' Você ainda não é cliente do Banco Agiplan? Então seja bem vindo! Você é funcionário público, aposentado ou pensionista?'
 
     return WebhookResponse(textoSaida, textoSaida, None, contextosDeSaida)
